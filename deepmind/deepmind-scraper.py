0d4c098e3b466e76109a2ca4ec7d1e22629c5beb
Stephen McAleese <stephenmcaleese11@gmail.com>
2022-07-01 17:27:14 +0100

    initial commit

---
+++ deepmind/deepmind-scraper.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def get_papers_and_authors(soup, papers_and_authors):
+    papers = soup.find_all('div', class_='c_card_list__item__container')
+    for paper in papers:
+        title = paper.find(attrs={'fs-cmsfilter-field': 'title'}).get_text()
+        print('title:', title)
+        author_elements = paper.find_all(attrs={'fs-cmsfilter-field': 'authors'})
+        for e in author_elements:
+            authors_string = e.get_text()
+            for char in ['\n', ' *']:
+                authors_string = authors_string.replace(char, '')
+            author_names = authors_string.split(',')
+        papers_and_authors[title] = author_names
+
+def main():
+    url = 'https://www.deepmind.com/research?tag=Safety&d907cb24_page=1'
+    papers_and_authors = {}
+    browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+    browser.get(url)
+    next_buttons = browser.find_elements(By.CLASS_NAME, 'c_card_list__pagination__children__link')
+
+    for i, button in enumerate(next_buttons):
+        print(f'clicked to page {i + 1}')
+        button.click()
+        time.sleep(1)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        get_papers_and_authors(soup, papers_and_authors)
+
+    print('papers and authors:', papers_and_authors, len(papers_and_authors.keys()))
+
+    df = pd.DataFrame({
+        'titles': papers_and_authors.keys(),
+        'authors': [', '.join(arr) for arr in papers_and_authors.values()]
+    })
+    df.to_csv('deepmind-papers-and-authors.csv')
+
+if __name__ == '__main__':
+    main()
---
+++ openai/openai-publication-scraping-script.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+from urllib.request import urlopen
+import pandas as pd
+
+'''
+DONE
+- get all the papers
+- open the CSV file and classify the papers as either related to safety or not
+
+TODO
+- get all the papers and authors 14:20
+'''
+
+def open_second_window(browser, link):
+    link.click()
+    sleep(3)
+    browser.switch_to.window(browser.window_handles[1])
+
+def close_second_window(browser):
+    browser.close()
+    browser.switch_to.window(browser.window_handles[0])
+
+def get_authors(browser, link):
+    open_second_window(browser, link)
+    author_links = browser.find_elements_by_css_selector('div.authors a')
+    if not author_links:
+        close_second_window(browser)
+        return ''
+    paper_authors = []
+    for link in author_links:
+        author = link.text
+        paper_authors.append(author)
+    author_string = ', '.join(paper_authors)
+    print('authors:', author_string)
+    close_second_window(browser)
+    return author_string
+
+def main():
+    url = 'https://openai.com/publications'
+    browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+    browser.get(url)
+
+    # get all the links from the page
+    links = browser.find_elements_by_css_selector('h5.medium-xsmall-copy a')
+    titles = []
+    authors = []
+
+    for link in links:
+        # 1. get title
+        title = link.text
+        titles.append(title)
+        print('title:', title)
+        # 2. get authors from arxiv
+        authors_of_paper = get_authors(browser, link)
+        authors.append(authors_of_paper)
+
+    df = pd.DataFrame({'titles': titles, 'authors': authors})
+    df.to_csv('openai-papers-and-authors.csv')
+
+if __name__ == '__main__':
+    main()
